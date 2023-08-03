d3a8fb7259558df15c1f1ec4d01ffef2be3dbc44
ZOMBEE1 <141204557+ZOMBEE1@users.noreply.github.com>
2023-08-03 06:35:06 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,106 +1,87 @@
 // Datos de ejemplo (camionetas)
-let camionetasOriginales = [
-  {
-    empresa: "Grupo Neria",
-    cabina: "Sencilla",
-    numero: "001",
-    modelo: "Modelo1",
-  },
-  {
-    empresa: "Super Pollo ",
-    cabina: "Doble",
-    numero: "101",
-    modelo: "Modelo2",
-  },
-  {
-    empresa: "Grupo Neria",
-    cabina: "Sencilla",
-    numero: "002",
-    modelo: "Modelo3",
-  },
-  {
-    empresa: "Super Pollo ",
-    cabina: "Doble",
-    numero: "102",
-    modelo: "Modelo4",
-  },
-  // Agrega más camionetas aquí si lo deseas
-];
+    // Datos de ejemplo (camionetas)
+    let camionetasOriginales = [
+      {
+        empresa: "Grupo Neria",
+        cabina: "Sencilla",
+        numero: "001",
+        modelo: "Modelo1",
+      },
+      {
+        empresa: "Super Pollo ",
+        cabina: "Doble",
+        numero: "101",
+        modelo: "Modelo2",
+      },
+      {
+        empresa: "Grupo Neria",
+        cabina: "Sencilla",
+        numero: "002",
+        modelo: "Modelo3",
+      },
+      {
+        empresa: "Super Pollo ",
+        cabina: "Doble",
+        numero: "102",
+        modelo: "Modelo4",
+      },
+      // Agrega más camionetas aquí si lo deseas
+    ];
 
-// Función para mostrar la lista de camionetas en la página
-function mostrarCamionetas() {
-  const camionetaSencillaGNList = document.getElementById(
-    "camionetaSencillaGNList"
-  );
-  const camionetaSencillaSPNList = document.getElementById(
-    "camionetaSencillaSPNList"
-  );
-  const camionetaDobleGNList = document.getElementById(
-    "camionetaDobleGNList"
-  );
-  const camionetaDobleSPNList = document.getElementById(
-    "camionetaDobleSPNList"
-  );
+    // Función para mostrar la lista de camionetas en la página
+    function mostrarCamionetas() {
+      const camionetaSencillaGNList = document.getElementById("camionetaSencillaGNList");
+      const camionetaSencillaSPNList = document.getElementById("camionetaSencillaSPNList");
+      const camionetaDobleGNList = document.getElementById("camionetaDobleGNList");
+      const camionetaDobleSPNList = document.getElementById("camionetaDobleSPNList");
 
-  // Limpiar las listas antes de volver a mostrar los resultados
-  camionetaSencillaGNList.innerHTML = "";
-  camionetaSencillaSPNList.innerHTML = "";
-  camionetaDobleGNList.innerHTML = "";
-  camionetaDobleSPNList.innerHTML = "";
+      // Limpiar las listas antes de volver a mostrar los resultados
+      camionetaSencillaGNList.innerHTML = "";
+      camionetaSencillaSPNList.innerHTML = "";
+      camionetaDobleGNList.innerHTML = "";
+      camionetaDobleSPNList.innerHTML = "";
 
-  let countSencillaSPN = 0;
-  let countDobleGN = 0;
+      let countSencillaSPN = 0;
+      let countDobleGN = 0;
 
-  camionetasOriginales.forEach((camioneta) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${camioneta.empresa} - Cabina ${camioneta.cabina} - Número ${camioneta.numero} - Modelo ${camioneta.modelo}`;
+      camionetasOriginales.forEach((camioneta) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${camioneta.empresa} - Cabina ${camioneta.cabina} - Número ${camioneta.numero} - Modelo ${camioneta.modelo}`;
 
-    if (
-      camioneta.empresa === "Grupo Neria" &&
-      camioneta.cabina === "Sencilla"
-    ) {
-      const camionetaSencillaGNListItem = document.createElement("li");
-      camionetaSencillaGNListItem.textContent = `${listItem.textContent}`;
-      camionetaSencillaGNList.appendChild(camionetaSencillaGNListItem);
-    } else if (
-      camioneta.empresa === "Super Pollo Co" &&
-      camioneta.cabina === "Sencilla"
-    ) {
-      countSencillaSPN++;
-      const listItemWithNumber = document.createElement("li");
-      listItemWithNumber.textContent = `${countSencillaSPN}. ${listItem.textContent}`;
-      camionetaSencillaSPNList.appendChild(listItemWithNumber);
-    } else if (
-      camioneta.empresa === "Grupo Neria" &&
-      camioneta.cabina === "Doble"
-    ) {
-      countDobleGN++;
-      const listItemWithNumber = document.createElement("li");
-      listItemWithNumber.textContent = `${countDobleGN}. ${listItem.textContent}`;
-      camionetaDobleGNList.appendChild(listItemWithNumber);
-    } else if (
-      camioneta.empresa === "Super Pollo Co" &&
-      camioneta.cabina === "Doble"
-    ) {
-      const camionetaDobleSPNListItem = document.createElement("li");
-      camionetaDobleSPNListItem.textContent = `${listItem.textContent}`;
-      camionetaDobleSPNList.appendChild(camionetaDobleSPNListItem);
+        if (camioneta.empresa === "Grupo Neria" && camioneta.cabina === "Sencilla") {
+          const camionetaSencillaGNListItem = document.createElement("li");
+          camionetaSencillaGNListItem.textContent = `${listItem.textContent}`;
+          camionetaSencillaGNList.appendChild(camionetaSencillaGNListItem);
+        } else if (camioneta.empresa === "Super Pollo " && camioneta.cabina === "Sencilla") {
+          countSencillaSPN++;
+          const listItemWithNumber = document.createElement("li");
+          listItemWithNumber.textContent = `${countSencillaSPN}. ${listItem.textContent}`;
+          camionetaSencillaSPNList.appendChild(listItemWithNumber);
+        } else if (camioneta.empresa === "Grupo Neria" && camioneta.cabina === "Doble") {
+          countDobleGN++;
+          const listItemWithNumber = document.createElement("li");
+          listItemWithNumber.textContent = `${countDobleGN}. ${listItem.textContent}`;
+          camionetaDobleGNList.appendChild(listItemWithNumber);
+        } else if (camioneta.empresa === "Super Pollo " && camioneta.cabina === "Doble") {
+          const camionetaDobleSPNListItem = document.createElement("li");
+          camionetaDobleSPNListItem.textContent = `${listItem.textContent}`;
+          camionetaDobleSPNList.appendChild(camionetaDobleSPNListItem);
+        }
+      });
     }
-  });
-}
 
-// Función para filtrar la lista de camionetas por número
-function filtrarPorNumero() {
-  const input = document.getElementById("searchInput");
-  const filterValue = input.value.toLowerCase();
-  camionetasOriginales = camionetasOriginales.filter((camioneta) =>
-    camioneta.numero.toLowerCase().includes(filterValue)
-  );
-  mostrarCamionetas();
-}
+    // Función para filtrar la lista de camionetas por número
+    function filtrarPorNumero() {
+      const input = document.getElementById("searchInput");
+      const filterValue = input.value.toLowerCase();
+      camionetasOriginales = camionetasOriginales.filter((camioneta) =>
+        camioneta.numero.toLowerCase().includes(filterValue)
+      );
+      mostrarCamionetas();
+    }
 
-document.addEventListener("DOMContentLoaded", mostrarCamionetas);
-document
+    document.addEventListener("DOMContentLoaded", mostrarCamionetas);
+    document.getElementById("searchInput").addEventListener("input", filtrarPorNumero);
   .getElementById("searchInput")
   .addEventListener("input", filtrarPorNumero);
   function setRotuladoCookie(camionetaId, rotulado) {
